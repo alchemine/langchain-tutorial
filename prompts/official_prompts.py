c3d851dbd630f052ada4c32a8ac48064ef899d7e
alchemine <djyoon0223@gmail.com>
2024-03-30 05:33:05 +0000

    chore: add poetry, docker environment

---
+++ prompts/official_prompts.py
@@ -0,0 +1,8 @@
+# from langchain.chains.openai_functions.tagging import _TAGGING_TEMPLATE
+_TAGGING_TEMPLATE = """Extract the desired information from the following passage.
+
+Only extract the properties mentioned in the 'information_extraction' function.
+
+Passage:
+{input}
+"""
---
+++ prompts/prompt_builder.py
@@ -0,0 +1,3 @@
+from langchain.prompts import PromptTemplate
+
+from prompts.official_prompts import _TAGGING_TEMPLATE
