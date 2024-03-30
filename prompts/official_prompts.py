1ad95805201a7066e313df33c29c386acf3e296a
alchemine <djyoon0223@gmail.com>
2024-03-30 16:05:37 +0000

    feat: add prompts

--- prompts/official_prompts.py
+++ prompts/official_prompts.py
@@ -1,8 +1,19 @@
 # from langchain.chains.openai_functions.tagging import _TAGGING_TEMPLATE
-_TAGGING_TEMPLATE = """Extract the desired information from the following passage.
+TAGGING_TEMPLATE = """Extract the desired information from the following passage.
 
 Only extract the properties mentioned in the 'information_extraction' function.
 
 Passage:
 {input}
 """
+
+
+# https://python.langchain.com/docs/modules/memory/custom_memory
+ENTITIES_INPUT_TEMPLATE = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know. You are provided with information about entities the Human mentions, if relevant.
+
+Relevant entity information:
+{entities}
+
+Conversation:
+Human: {input}
+AI:"""
--- prompts/prompt_builder.py
+++
@@ -1,3 +0,0 @@
-from langchain.prompts import PromptTemplate
-
-from prompts.official_prompts import _TAGGING_TEMPLATE
